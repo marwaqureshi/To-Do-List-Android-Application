44da445e5c84b07b917e2530a9c121f5b70617bc
BryceMcNary <mcnary.4@wright.edu>
2023-02-07 20:45:55 -0500

    Add Task Class

---
+++ app/src/main/java/com/example/todolist/Model/Task.java
@@ -0,0 +1,50 @@
+package com.example.todolist.Model;
+
+public class Task {
+    private int taskId, isComplete;
+    private String taskName, taskDescription, taskDate;
+
+    public Task(){
+
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public int getIsComplete() {
+        return isComplete;
+    }
+
+    public void setIsComplete(int isComplete) {
+        this.isComplete = isComplete;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getTaskDate() {
+        return taskDate;
+    }
+
+    public void setTaskDate(String taskDate) {
+        this.taskDate = taskDate;
+    }
+}
