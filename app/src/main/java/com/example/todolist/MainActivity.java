1c59b9147197d5817c24c68908932cde5f143d84
jaysstewart <jay.stewart451@gmail.com>
2023-02-15 03:36:03 -0500

    Added database to locally store persistent data

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.todolist.Model.AppDatabase;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -13,6 +14,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import com.example.todolist.databinding.ActivityMainBinding;
 
@@ -21,10 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    public static AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "tasks-db").allowMainThreadQueries().build();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -51,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -62,5 +73,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public AppDatabase getDb() {
+        return db;
     }
 }
--- app/src/main/java/com/example/todolist/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/todolist/ui/home/HomeFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,17 +12,20 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.todolist.MainActivity;
+import com.example.todolist.Model.AppDatabase;
 import com.example.todolist.Model.Task;
+import com.example.todolist.Model.TaskDao;
 import com.example.todolist.R;
 import com.example.todolist.databinding.FragmentHomeBinding;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class HomeFragment extends Fragment {
 
+    AppDatabase db;
+    TaskDao taskDao;
+
     private FragmentHomeBinding binding;
-    List<Task> taskList = new ArrayList<>();
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -43,33 +45,31 @@ public class HomeFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+
+        db = MainActivity.db;
+        taskDao = db.taskDao();
+
         recycler();
     }
 
     public void addToRecycler(Task task) {
-        taskList.add(task);
+        taskDao.insert(task);
         RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskList));
+        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskDao.getAll()));
     }
 
     public void removeFromRecycler(Task task){
-        taskList.remove(task);
+        taskDao.delete(task);
         RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskList));
+        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskDao.getAll()));
     }
 
     public void recycler() {
-
-        //TODO replace with list from local storage, Populate list for testing
-        taskList.add(new Task(1, R.drawable.placeholder,true, "Task1", "Descrip1", "1/1/2023"));
-        taskList.add(new Task(2, R.drawable.placeholder,true, "Task2", "Descrip2", "1/1/2023"));
-        taskList.add(new Task(3, R.drawable.placeholder,true, "Task3", "Descrip3", "1/1/2023"));
-
         RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskList));
+        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskDao.getAll()));
     }
 
     @Override
