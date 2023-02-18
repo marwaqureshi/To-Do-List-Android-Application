204544c7d156776fe616c41a3d2f8cbf15704eb1
BryceMcNary <mcnary.4@wright.edu>
2023-02-18 15:27:45 -0500

    Add OnItemClicked listener for RecyclerView task containers
    The RecyclerView task containers now open bottom_sheet_layout Dialog when clicked. The bottom_sheet_layout Dialog presents three options: markComplete, edit, delete. onClickListeners for these three LinearLayouts are located in showDialog(Task task) method of HomeFragment.java

--- app/src/main/java/com/example/todolist/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/todolist/ui/home/HomeFragment.java
@@ -1,10 +1,18 @@
 package com.example.todolist.ui.home;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,7 +28,7 @@ import com.example.todolist.databinding.FragmentHomeBinding;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements SelectListener{
 
     private FragmentHomeBinding binding;
     List<Task> taskList = new ArrayList<>();
@@ -50,14 +58,14 @@ public class HomeFragment extends Fragment {
         taskList.add(task);
         RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskList));
+        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskList, this));
     }
 
     public void removeFromRecycler(Task task){
         taskList.remove(task);
         RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskList));
+        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskList, this));
     }
 
     public void recycler() {
@@ -69,12 +77,61 @@ public class HomeFragment extends Fragment {
 
         RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskList));
+        recyclerView.setAdapter(new RecyclerAdapter(getContext(),taskList,this));
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    //onClick listener for recycler view
+    public void onItemClicked(Task task) {
+        showDialog(task);
+    }
+
+    private void showDialog(Task task) {
+        //declare Dialog and set dialog view to bottom_sheet_layout
+        final Dialog dialog = new Dialog(getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_sheet_layout);
+
+        //declare LinearLayouts from bottom_sheet_layout
+        LinearLayout markCompleteLayout = dialog.findViewById(R.id.bottom_sheet_markComplete);
+        LinearLayout editLayout = dialog.findViewById(R.id.bottom_sheet_edit);
+        LinearLayout deleteLayout = dialog.findViewById(R.id.bottom_sheet_delete);
+
+        // create onClickListener for each LinearLayout
+        markCompleteLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Update the selected task to present as completed
+                Toast.makeText(getContext(), "Mark " + task.getTaskName() + " as complete", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        editLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Make this ReOpen the ADD Task view and allow user to modify/update task details (or cancel)
+                Toast.makeText(getContext(), "edit " + task.getTaskName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        deleteLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Make this open a pop-up "are you sure?" window and if confirm, delete the task
+                Toast.makeText(getContext(), "delete " + task.getTaskName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //start dialog and display on screen
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 }
