843c1d6f593bd2ad77c55e337a480bd6096ef207
Marwa Qureshi <marwaqureshi1@gmail.com>
2023-03-28 18:44:47 -0400

    Disable EditText of Today's Date when it gains focus and will remain grey throughout creating task#65

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -90,62 +90,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Set up the due date format and restriction
-        EditText dueDateEditText = findViewById(R.id.due_date);
-        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
-
-        dueDateEditText.addTextChangedListener(new TextWatcher() {
-            private String current = "";
-            private String mmddyyyy = "MMDDYYYY";
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (!s.toString().equals(current)) {
-                    String clean = s.toString().replaceAll("[^\\d.]", "");
-                    String cleanC = current.replaceAll("[^\\d.]", "");
-
-                    int cl = clean.length();
-                    int sel = cl;
-                    for (int i = 2; i <= cl && i < 6; i += 2) {
-                        sel++;
-                    }
-                    //Fix for pressing delete next to a forward slash
-                    if (clean.equals(cleanC)) sel--;
-
-                    if (clean.length() < 8) {
-                        clean = clean + mmddyyyy.substring(clean.length());
-                    } else {
-                        //This part makes sure that when we finish entering numbers
-                        //the date is correct, fixing it otherwise
-                        int month = Integer.parseInt(clean.substring(0, 2));
-                        int day = Integer.parseInt(clean.substring(2, 4));
-                        int year = Integer.parseInt(clean.substring(4, 8));
-
-                        month = month < 1 ? 1 : month > 12 ? 12 : month;
-                        Calendar cal = Calendar.getInstance();
-                        cal.set(Calendar.MONTH, month - 1);
-                        year = (year < 1900) ? 1900 : (year > cal.get(Calendar.YEAR)) ? cal.get(Calendar.YEAR) : year;
-                        cal.set(Calendar.YEAR, year);
-                        day = (day > cal.getActualMaximum(Calendar.DATE)) ? cal.getActualMaximum(Calendar.DATE) : day;
-                        clean = String.format("%02d%02d%02d", month, day, year);
-                    }
-
-                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
-                            clean.substring(2, 4),
-                            clean.substring(4, 8));
-
-                    sel = sel < 0 ? 0 : sel;
-                    current = clean;
-                    dueDateEditText.setText(current);
-                    dueDateEditText.setSelection(sel < current.length() ? sel : current.length());
-                }
-            }
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
-
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
